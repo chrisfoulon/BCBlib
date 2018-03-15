81cfd0becd8814b6befb7854bacb2b2c6641ccb1
chrisfoulon <hd.chrisfoulon@gmail.com>
2018-03-15 17:17:31 +0100

    Changed the structure to correctly upload it on pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='bcblib',        # This is the name of your PyPI-package.
     version='0.1.0',     # Update the version number for new releases
     data_files=[('priors', ['../Data/ants_priors/brainPrior.nii.gz'])],
     keywords='brain neuroimaging',
-    packages=find_packages(),
-    install_requires=['nibabel numpy'],
+    packages=find_packages(exclude=['__pycache__']),
+    install_requires=['nibabel', 'numpy'],
     project_urls={  # Optional
         'Source': 'https://github.com/chrisfoulon/BCBlib',
         'Bug Reports': 'https://github.com/chrisfoulon/BCBlib/issues',
