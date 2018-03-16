df43d2c6461e8d7fe20f903072f4c352f3640ddb
chrisfoulon <hd.chrisfoulon@gmail.com>
2018-03-16 11:34:22 +0100

    Version of the setup updated and added init and license file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bcblib',        # This is the name of your PyPI-package.
-    version='0.1.0',     # Update the version number for new releases
+    version='0.1.1',     # Update the version number for new releases
     data_files=[('priors', ['../Data/ants_priors/brainPrior.nii.gz'])],
     keywords='brain neuroimaging',
     packages=find_packages(exclude=['__pycache__']),
