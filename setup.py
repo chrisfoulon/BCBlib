35dda0fa9a7016e9ae481e8bd0ab30e75344d807
Foulon <cf27246@dhcp-10-157-81-43.dellmedschool.utexas.edu>
2019-03-28 14:12:34 -0500

    It seems I should not include core packages in the package requirements

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.rst", "r") as fh:
 
 setup(
     name='bcblib',        # This is the name of your PyPI-package.
-    version='0.1.1',     # Update the version number for new releases
+    version='0.1.2',     # Update the version number for new releases
     # data_files=[('priors', ['../Data/ants_priors/brainPrior.nii.gz'])],
     keywords='brain neuroimaging',
     packages=find_packages(exclude=['__pycache__']),
-    install_requires=['nibabel', 'numpy', 'six', 'os', 'sys', 'errno', 'scipy'],
+    install_requires=['nibabel', 'numpy', 'six', 'scipy'],
     project_urls={  # Optional
         'Source': 'https://github.com/chrisfoulon/BCBlib',
         'Bug Reports': 'https://github.com/chrisfoulon/BCBlib/issues',
