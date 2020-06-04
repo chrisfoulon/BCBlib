46f2d69bcd31142ae5217ab43afdade118ead24a
chrisfoulon <tolhsadum@gmail.com>
2020-06-04 16:40:17 +0100

    cleaned and updated the distribution files

--- README.rst
+++ README.rst
@@ -3,3 +3,5 @@ BCBlib: neuroimaging analyses
 =============================
 
 Under development
+
+Repository of scripts and tools used for MRI data analysis
--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 from setuptools import setup, find_packages
+import os
 
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='bcblib',        # This is the name of your PyPI-package.
-    version='0.1.2',     # Update the version number for new releases
+    version='0.2',     # Update the version number for new releases
     # data_files=[('priors', ['../Data/ants_priors/brainPrior.nii.gz'])],
     keywords='brain neuroimaging',
+    long_description=read('README.rst'),
+    zip_safe=True,
+    include_package_data=True,
     packages=find_packages(exclude=['__pycache__']),
-    install_requires=['nibabel', 'numpy', 'six', 'scipy'],
+    install_requires=['nibabel', 'numpy', 'six', 'scipy', 'nilearn', 'sklearn'],
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst"],
+        # Include all the executables from bin
+        "Data": ["*"],
+        "bash": ["*"],
+    },
+    entry_points={
+        'console_scripts': ['generate_synth_lesions = bcblib.scripts.generate_synth_lesions:main',
+                            'pick_up_matched_synth_lesions = bcblib.scripts.pick_up_synth_lesions:main']
+        # 'console_scripts': ['dicom_conversion = data_identification.scripts.dicom_conversion:convert']
+    },
     project_urls={  # Optional
         'Source': 'https://github.com/chrisfoulon/BCBlib',
         'Bug Reports': 'https://github.com/chrisfoulon/BCBlib/issues',
