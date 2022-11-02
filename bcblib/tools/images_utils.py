86a4494bdde71cf39c8e4d7ce69cb8f51161e637
chrisfoulon <tolhsadum@gmail.com>
2022-11-02 11:54:54 +0000

    added doc descriptions

--- bcblib/tools/images_utils.py
+++ bcblib/tools/images_utils.py
@@ -8,6 +8,17 @@ from bcblib.tools.nifti_utils import load_nifti
 
 
 def get_mask_perimeter(array, binarise_thr=0.5):
+    """
+    Returns a binary mask of the input mask's perimeter with a thickness of one voxel
+    Parameters
+    ----------
+    array
+    binarise_thr
+
+    Returns
+    -------
+
+    """
     bin_array = np.where(array >= binarise_thr, 1, 0)
     erode_array = np.copy(bin_array)
     erode_array = binary_erosion(erode_array)
@@ -16,6 +27,19 @@ def get_mask_perimeter(array, binarise_thr=0.5):
 
 
 def dilate_mask(array, connectivity, dimensions=3):
+    """
+    Dilates a mask using a structure determined by the connectivity (it can be a cube, a cross etc ...)
+    structure ref: https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.generate_binary_structure.html
+    Parameters
+    ----------
+    array
+    connectivity
+    dimensions
+
+    Returns
+    -------
+    dilated array with the same type as input array
+    """
     binary_structure = generate_binary_structure(dimensions, connectivity)
     return binary_dilation(array, binary_structure).astype(array.dtype)
 
@@ -30,6 +54,20 @@ def get_masked_img_min_max_range(mask_arr):
 
 
 def get_roi_intensity_range(image, mask, dilation_connectivity=2, binarise_thr=0.5):
+    """
+    Calculate the perimeter of the mask, dilate it and then extracts the intensity of the input image withing this mask
+    to compute the min-max range
+    Parameters
+    ----------
+    image
+    mask
+    dilation_connectivity
+    binarise_thr
+
+    Returns
+    -------
+
+    """
     image_hdr = load_nifti(image)
     image_data = image_hdr.get_fdata()
     mask_hdr = load_nifti(mask)
