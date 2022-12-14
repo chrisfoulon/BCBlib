334fe2bc7a1b69291175297eb65b481d51fb1f5d
chrisfoulon <tolhsadum@gmail.com>
2022-12-14 10:03:22 +0000

    fixed it

--- bash/tools/randomise_folders.sh
+++ bash/tools/randomise_folders.sh
@@ -4,10 +4,16 @@
   $2 : number of permutations (default 10000)
   [$3 : prefix of the input file for randomise inside the subfolders]'; exit 1; }
 
+dir=$1
+
+if [[ ! $dir =~ /$ ]];
+then
+  dir="$dir/"
+fi
 
 # randomise -i toto_filtered_4D.nii.gz -o toto_filtered_4D.nii.gz -d toto_design.mat -t design.con -T -n 10000
-echo "Starting $1"
-for d in "$1"/*/;
+echo "Starting $dir"
+for d in "$dir"*/;
 do
   cd "$d" || echo "Could not change directory in $d"
 #  test if the files exist
