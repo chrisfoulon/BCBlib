2108992e9bb6870530787cc522ef0527a7a38c01
chrisfoulon <tolhsadum@gmail.com>
2023-03-09 15:41:36 +0000

    Added function to split a dictionnary in smaller dictionaries

--- bcblib/tools/general_utils.py
+++ bcblib/tools/general_utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 import numpy as np
 
@@ -15,3 +16,42 @@ def save_json(path, d):
 
 def save_list(path, li):
     np.savetxt(path, li, delimiter='\n', fmt='%s')
+
+
+def split_dict(d, chunk_size, output_dir=None, output_pref=None):
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(exist_ok=True)
+        if not output_pref:
+            output_pref = 'chunk'
+        # Check whether the output_dir does not contain files with the same prefix followed by _ and a number and .json
+        for f in output_dir.iterdir():
+            if f.name.startswith(output_pref) and f.name.endswith('.json'):
+                try:
+                    int(f.name.split('_')[-1].split('.')[0])
+                    print(f'ERROR: Output directory {output_dir} already contains files matching the output filenames')
+                    return
+                except ValueError:
+                    pass
+
+    print(f'Length of the input dictionary: {len(d)}')
+    keys_list = list(d.keys())
+    i = -1
+    end_index = 0
+    for i in range(len(keys_list) // chunk_size):
+        begin_index = chunk_size * i
+        end_index = chunk_size * (1 + i)
+        if output_dir is not None:
+            save_json(output_dir.joinpath(f'{output_pref}_{i}.json'),
+                      {k: d[k] for k in keys_list[begin_index:end_index]})
+        print(len({k: d[k] for k in keys_list[begin_index:end_index]}))
+    print(len({k: d[k] for k in keys_list[end_index:]}))
+    save_json(output_dir.joinpath(f'{output_pref}_{i + 1}.json'),
+              {k: d[k] for k in keys_list[end_index:]})
+
+"""
+Continue the comment with the commands to upload this package to pypi:
+python setup.py sdist bdist_wheel
+twine upload dist/*
+
+"""
